b93c8b7c777e2992054e18fe0e598e6d31c03f13
Caleb Cordes <69608629+cmcordes@users.noreply.github.com>
2020-12-10 17:46:15 -0600

    This works?

--- matlab_scripts/script_2.m
+++ matlab_scripts/script_2.m
@@ -3,16 +3,9 @@
 %   This script produces the second letter of the logo.
 %
 
-%read in data
-data = csvread('data/letter_2.csv');
-%plot data
-l = patch(data(:,1), data(:,2), 'black');
-l.LineWidth = 2;
 
-%% SOLUTION: Breakout 1
-
-% %read in data
-% data = csvread('data/letter_2.csv');
-% %plot data
-% l = patch(data(:,1), data(:,2), [0, 0.2, 0.4]);  % per reviewer comment
-% l.LineWidth = 2;
+ %read in data
+ data = csvread('data/letter_2.csv');
+ %plot data
+ l = patch(data(:,1), data(:,2), [0, 0.2, 0.4]);  % per reviewer comment
+ l.LineWidth = 2;
